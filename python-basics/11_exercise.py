f7c85d54e15828f0c0ff5e47521c956d2433dca2
codesterlalit <laalit.sunaar@gmail.com>
2026-08-27 00:30:20 +0100

    feat: exercise 11

--- python-basics/11_exercise.py
+++ python-basics/11_exercise.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr, ValidationError
 
-class UserInput(BaseModel):
-    name: str = Field(min_length=1)
-    age: int = Field(ge=0, le=130)
+# class UserInput(BaseModel):
+#     name: str = Field(min_length=1)
+#     age: int = Field(ge=0, le=130)
 
-user = UserInput.model_validate({"name": "Ada", "age": 36})
-print(user.model_dump())
+# user = UserInput.model_validate({"name": "Ada", "age": 36})
+# print(user.model_dump())
+
+class EmailRequest(BaseModel):
+    subject: str = Field(..., min_length=1, description="Subject cannot be empty")
+    body: str = Field(..., max_length=10_000, description="Body must not exceed 10000 character.")
+    recipient: EmailStr = Field(..., description="Must be valid email address." )
+
+try:
+    newEmail = EmailRequest.model_validate({"subject": "test", "body": "tesing", "recipient": "laalit@gmail.com"})
+    print(newEmail.model_dump())
+except ValidationError as error:
+    print(error)
